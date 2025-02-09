13928f10894206ec91a904a1305aeecd2c519774
jsbonilla2 <JairoBonilla2004@users.noreply.github.com>
2025-02-09 18:58:53 -0500

    delete

--- Ejercicio_web/Ejercicio_web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Ejercicio_web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
